7b4e6c9ab8f2f266ce5ef2b03bc5ac59cb4920dd
John Kim <jkim@rowlandschools.org>
2019-11-16 15:25:07 -0800

    2019-11-16
    Movie

---
+++ Movie.java
@@ -0,0 +1,24 @@
+
+/**
+ * A Class to hold Movie Facts
+ *
+ * @author (Mr. Kim)
+ * @version (2019-11-16)
+ */
+public class Movie
+{
+    // instance variables - replace the example below with your own
+    private String name;
+    private String studio;
+    private int    earnings;
+    private int    year;
+
+    /**
+     * Constructor for objects of class Movie
+     */
+    public Movie()
+    {
+
+    }
+
+}
---
+++ MovieListsAR.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+/**
+ * Write a description of class MovieListsAL here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MovieListsAR
+{
+    // instance variables - replace the example below with your own
+    private Movie[] pool;
+
+    /**
+     * Constructor for objects of class MovieListsAL
+     */
+    public MovieListsAR()
+    {
+
+    }
+
+
+}
