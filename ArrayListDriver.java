8e67c8b4bc27b022e24bf354f089c5eabfc4f06e
John Kim <jkim@rowlandschools.org>
2019-11-16 09:59:41 -0800

    2019-11-16
    Started Project

---
+++ ArrayListDriver.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+
+/**
+ * Write a description of class ArrayListDriver here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ArrayListDriver
+{
+    
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        nums.add(1);
+        nums.add(4);
+        nums.add(9);
+        System.out.println(nums.toString());
+    }
+    
+    
+    
+    public static ArrayList<Integer> case1(ArrayList<Integer> a)
+    {
+        
+        return null;
+    }
+}
